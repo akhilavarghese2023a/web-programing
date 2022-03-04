f414f9dd2bdb111d8c9b0aea226cd38b4929cb98
akhilavarghese2023a <97580294+akhilavarghese2023a@users.noreply.github.com>
2022-03-04 12:08:15 +0530

    Add files via upload

---
+++ 4-12-21/multidimenarray_key.php
@@ -0,0 +1,7 @@
+<?php
+$students=array(array("name"=>"Rahul","Age"=>19),
+		array("name"=>"Vineeth","Age"=>32),
+		array("name"=>"Sahal","Age"=>23),
+		array("name"=>"Rafi","Age"=>34));
+print_r($students);
+?>
